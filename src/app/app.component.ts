0f1198ce41f89b352e2ae3a8a2860e4e46e1f5bb
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-08 21:54:39 +0700

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-title></app-title>
+    <img alt='logo' [style.width.px]='imageWidth' src={{angularImg}} />
+    <img alt='logo' [style.width.px]='imageWidth' [src]='reactImg' />
+    <img alt='logo' [style.width.px]='imageWidth' [src]='vueImg' />
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular';
+  angularImg = 'https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-svg-vector.svg';
+  reactImg = 'https://cdn.freebiesupply.com/logos/large/2x/react-logo-png-transparent.png';
+  vueImg = 'https://vuejs.org/images/logo.png';
+
+  imageWidth = 200;
 }
--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   template: `
-    <h1>How Cuongw learn Angular?</h1>
-  `
+    <h1 [class.with-underline]='withUnderline' [ngStyle]='{color: color,fontWeight: fontWeight}'>How Cuongw learn Angular?</h1>
+  `,
+  styleUrls: ['./title.component.css']
 })
-export class TitleComponent {}
+export class TitleComponent {
+  withUnderline = true;
+  color = 'tomato';
+  fontWeight = 'lighter';
+}
